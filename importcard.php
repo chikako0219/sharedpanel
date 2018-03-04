bd46bc66d255bf3ef051eb10a20e474cc1c19ae1
yuesan <yae.yue@gmail.com>
2018-03-04 22:52:43 +0900

    Fix bug

--- importcard.php
+++ importcard.php
@@ -57,26 +57,26 @@ echo $OUTPUT->header();
 $cardidstwitter = $cardidsemails = $cardidsevernote = $cardidsfacebook = null;
 
 // Twitter.
-//$twitterobj = new twitter($sharedpanel);
-//if ($twitterobj->is_enabled()) {
-//    $cardidstwitter = $twitterobj->import();
-//    if ($cardidstwitter != false || is_null($cardidstwitter) || is_array($cardidstwitter)) {
-//        if (count($cardidstwitter) == 0) {
-//            echo html_writer::message(notification::INFO, get_string('import_twitter_no_tweets', 'mod_sharedpanel'));
-//        } else {
-//            $str = new \stdClass();
-//            $str->source = 'Twitter';
-//            $str->count = count($cardidstwitter);
-//            echo html_writer::message(notification::SUCCESS, get_string('import_success', 'mod_sharedpanel', $str));
-//        }
-//    } else {
-//        echo html_writer::message(notification::ERROR, get_string('import_failed', 'mod_sharedpanel', 'Twitter'));
-//        $error = $twitterobj->get_error();
-//        debugging($error->code . ":" . $error->message);
-//    }
-//} else {
-//    echo html_writer::message(notification::INFO, get_string('import_no_authinfo', 'mod_sharedpanel', 'Twitter'));
-//}
+$twitterobj = new twitter($sharedpanel);
+if ($twitterobj->is_enabled()) {
+    $cardidstwitter = $twitterobj->import();
+    if ($cardidstwitter != false || is_null($cardidstwitter) || is_array($cardidstwitter)) {
+        if (count($cardidstwitter) == 0) {
+            echo html_writer::message(notification::INFO, get_string('import_twitter_no_tweets', 'mod_sharedpanel'));
+        } else {
+            $str = new \stdClass();
+            $str->source = 'Twitter';
+            $str->count = count($cardidstwitter);
+            echo html_writer::message(notification::SUCCESS, get_string('import_success', 'mod_sharedpanel', $str));
+        }
+    } else {
+        echo html_writer::message(notification::ERROR, get_string('import_failed', 'mod_sharedpanel', 'Twitter'));
+        $error = $twitterobj->get_error();
+        debugging($error->code . ":" . $error->message);
+    }
+} else {
+    echo html_writer::message(notification::INFO, get_string('import_no_authinfo', 'mod_sharedpanel', 'Twitter'));
+}
 
 // Email.
 $emailobj = new email($sharedpanel);
